5cd9c2fd2b0d43ebc5e3b11151f62fb841437472
Dariusz Lozynski <dariusz.lozynski@icloud.com>
2025-01-23 13:30:44 +0100

    fix typo Input leap cask

--- Casks/input-leap.rb
+++ Casks/input-leap.rb
@@ -5,7 +5,7 @@ cask "input-leap" do
   sha256 arm "e7a27f187e4e97f724e7b0ae9f9490be27d3e81a6cac0e3cf85654b37f28b1a3",
          intel: "7d335ff7be060d378eac55b792f4d256ac6210c6c0a8e35147c10c799059f8e7"
 
-  url "https://github.com/input-leap/input-leap/releases/download/v#{version}/InputLeap_#{version}-macos_#{arch}.dmg"
+  url "https://github.com/input-leap/input-leap/releases/download/v#{version}/InputLeap_#{version}_macos_#{arch}.dmg"
   name "InputLeap"
   desc "Open-source KVM software"
   homepage "https://github.com/input-leap/input-leap"
