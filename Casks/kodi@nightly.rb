24efd5e60e39f6f72fd31383618d95d765b825fd
Dariusz Łożyński <dlozynski@users.noreply.github.com>
2025-03-26 13:23:43 +0100

    Update kodi@nightly

--- Casks/kodi@nightly.rb
+++ Casks/kodi@nightly.rb
@@ -1,16 +1,27 @@
 cask "kodi@nightly" do
-
   version "master"
-  date "20250325"
-  commit "71734296"
-
-
   sha256 :no_check
-
   arch arm: "arm64", intel: "x86_64"
-  build_number arm: "8126", intel: "37850"
 
-  url "https://mirrors.kodi.tv/nightlies/osx/#{arch}/#{version}/kodi-#{date}-#{commit}-#{version}-#{build_number}-#{arch}.dmg"
+  @@revision="71734296"
+  @@date="20250325"
+  @@os="osx"
+  @@build_number="00000"
+  @@extension="dmg"
+
+  on_macos do
+    @@extension="dmg"
+    @os="osx"
+    if Hardware::CPU.arm?
+      @@build_number="8126"
+    end
+    if Hardware::CPU.intel?
+      @@build_number="37850"
+    end
+  end
+
+  url "https://mirrors.kodi.tv/nightlies/osx/#{arch}/#{version}/kodi-#{@@date}-#{@@revision}-#{version}-#{@@build_number}-#{arch}.#{@@extension}"
+  
   name "Kodi Nightly"
   desc "Free and open-source media player"
   homepage "https://kodi.tv/"
