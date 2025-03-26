0bcdcd5452fedf32a2da53b583ff01bc3848b2c8
Dariusz Łożyński <dlozynski@users.noreply.github.com>
2025-03-26 13:39:39 +0100

    Fix kodi nightly (#10)

--- Casks/kodi@nightly.rb
+++ Casks/kodi@nightly.rb
@@ -1,16 +1,31 @@
 cask "kodi@nightly" do
 
   version "master"
-  date "20250325"
-  commit "71734296"
-
-
   sha256 :no_check
-
   arch arm: "arm64", intel: "x86_64"
-  build_number arm: "8126", intel: "37850"
 
-  url "https://mirrors.kodi.tv/nightlies/osx/#{arch}/#{version}/kodi-#{date}-#{commit}-#{version}-#{build_number}-#{arch}.dmg"
+  @@os="osx"
+  @@extension="dmg"
+  @@build_number="00000"
+  # Update those values
+  @@date="20250325"
+  @@revision="71734296"
+
+  on_macos do
+    @@extension="dmg"
+    @os="osx"
+    if Hardware::CPU.arm?
+      # Update this value
+      @@build_number="8126"
+    end
+    if Hardware::CPU.intel?
+      # Update this value
+      @@build_number="37850"
+    end
+  end
+
+  url "https://mirrors.kodi.tv/nightlies/osx/#{arch}/#{version}/kodi-#{@@date}-#{@@revision}-#{version}-#{@@build_number}-#{arch}.#{@@extension}"
+  
   name "Kodi Nightly"
   desc "Free and open-source media player"
   homepage "https://kodi.tv/"
@@ -26,9 +41,10 @@ cask "kodi@nightly" do
 
   app "Kodi.app"
 
+  # No trashing for user data for nightly builds
+  #"~/Library/Application Support/Kodi",  
   zap trash: [
     "~/.kodi",
-    "~/Library/Application Support/Kodi",
     "~/Library/Logs/kodi.log",
     "~/Library/Logs/kodi.old.log",
     "~/Library/Preferences/org.xbmc.kodi.plist",
