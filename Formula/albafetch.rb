1f543b7efbf8c913e865160e07044b6f5ec22312
Dariusz Lozynski <dariusz.lozynski@icloud.com>
2025-03-28 14:53:16 +0100

    fix alba

--- Formula/albafetch.rb
+++ Formula/albafetch.rb
@@ -1,11 +1,10 @@
 class Albafetch < Formula
-  name "albafetch"
-  version "4.2.1"
-  arch arm: "AppleSilicon", intel: "x86_64"
   desc "Faster neofetch alternative, written in C. Still improving :)"
   homepage "https://github.com/alba4k/albafetch/"
+  version "4.2.1"
   url "https://github.com/alba4k/albafetch/archive/refs/tags/v#{version}.tar.gz"
   sha256 "9c691e245a0f4ad11eba82852d4f55eaeacfebef260c029145bea64581f911df"
+
   license "MIT"
   head "https://github.com/alba4k/albafetch.git", branch: "master"
 
@@ -18,8 +17,8 @@ class Albafetch < Formula
   depends_on "meson" => :build
 
   def install
-    system "meson", "setup", "build"
-    system "meson", "compile", "-C", "build"
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
 
