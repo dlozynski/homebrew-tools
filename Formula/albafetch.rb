18b019b289f77614c0f4650ab0b41bb9acdc43a4
Dariusz Łożyński <dlozynski@users.noreply.github.com>
2025-03-28 14:10:28 +0100

    Feature/albafetch formula (#15)

---
+++ Formula/albafetch.rb
@@ -0,0 +1,29 @@
+class Albafetch < Formula
+  name "albafetch"
+  version "4.2.1"
+  arch arm: "AppleSilicon", intel: "x86_64"
+  desc "Faster neofetch alternative, written in C. Still improving :)"
+  homepage "https://github.com/alba4k/albafetch/"
+  url "https://github.com/alba4k/albafetch/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "9c691e245a0f4ad11eba82852d4f55eaeacfebef260c029145bea64581f911df"
+  license "MIT"
+  head "https://github.com/alba4k/albafetch.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/[^"' >]*?v?(\d+(?:\.\d+)+)[^"' >]*?/i)
+    strategy :github_latest
+  end
+
+  depends_on "meson" => :build
+
+  def install
+    system "meson", "setup", "build"
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    assert_equal "albafetch - a system fetch utility (#{version})", pipe_output("#{bin}/albafetch -h | grep #{version}")
+  end
+end
